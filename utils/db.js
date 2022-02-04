cdb11d9b84b2fbb18475bb74bc2110fd9f477142
samwalkers <samuelsafahi@icloud.com>
2022-02-03 16:51:17 -0800

    a

--- utils/db.js
+++ utils/db.js
@@ -4,7 +4,7 @@ const MongoClient = require('mongodb').MongoClient
 
 const initializeClient = () => {
     try {
-        const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.it21v.mongodb.net/fomo?retryWrites=true&w=majority`
+        const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.w7jad.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
         const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true  })
         client.connect()
         const getClient = () => client
@@ -17,7 +17,7 @@ const initializeClient = () => {
 
 const { client, getClient } = initializeClient()
 
- 
+
 updateprojectOneData = async (chainData, client) => {
     try {
         const database = client.db('fomo')
@@ -25,11 +25,11 @@ updateprojectOneData = async (chainData, client) => {
         chainData.timestamp = Date.now()
         let newChainData = Object.assign({}, chainData)
         newChainData._id = ObjectId()
-        let deleteResult = await collection.deleteMany( { timestamp : {"$lt" : Date.now() - 60 * 1000 }}) 
+        let deleteResult = await collection.deleteMany( { timestamp : {"$lt" : Date.now() - 60 * 1000 }})
         console.log(`Deleted: ${deleteResult.deletedCount}`)
         await collection.insertOne(newChainData)
         console.log("Inserted new entries")
-    } 
+    }
     catch(err) {
         console.log(err)
     }
@@ -43,11 +43,11 @@ updateprojectTwoData = async (chainData, client) => {
         chainData.timestamp = Date.now()
         let newChainData = Object.assign({}, chainData)
         newChainData._id = ObjectId()
-        let deleteResult = await collection.deleteMany( { timestamp : {"$lt" : Date.now() - 60 * 1000 }}) 
+        let deleteResult = await collection.deleteMany( { timestamp : {"$lt" : Date.now() - 60 * 1000 }})
         console.log(`Deleted: ${deleteResult.deletedCount}`)
         await collection.insertOne(newChainData)
         console.log("Inserted new entries")
-    } 
+    }
     catch(err) {
         console.log(err)
     }
@@ -59,8 +59,8 @@ getCachedprojectOneData = async (client) => {
         const collection = database.collection('projectOne')
         cachedData = await collection.find().sort({ _id: -1 }).limit(1).toArray()
         cachedData = cachedData[0]
-        return cachedData    
-    } 
+        return cachedData
+    }
     catch(err) {
         console.log(err)
     }
@@ -73,8 +73,8 @@ getCachedprojectTwoData = async (client) => {
         const collection = database.collection('projectTwo')
         cachedData = await collection.find().sort({ _id: -1 }).limit(1).toArray()
         cachedData = cachedData[0]
-        return cachedData    
-    } 
+        return cachedData
+    }
     catch(err) {
         console.log(err)
     }
@@ -88,4 +88,3 @@ module.exports = {
     updateprojectTwoData,
     getCachedprojectTwoData
 }
-
